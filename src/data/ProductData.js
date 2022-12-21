fe23b4da50e408d4f69b996e376f7f19dba04e4d
jacob-d242 <jacobmoracha@gmail.com>
2022-12-21 10:49:49 +0300

    Adding image to prducts data

--- src/data/ProductData.js
+++ src/data/ProductData.js
@@ -5,31 +5,35 @@ const gasData = [
         name: 'SeaGas',
         Size: '6',
         price: '1200',
-        image:'https'
+        image: 'https://res.cloudinary.com/digem679d/image/upload/v1668459527/d2cvxdvgs1oenqyo8xyn.jpg'
     },
     {
         id: '2',
         name: 'Total',
         Size: '6',
-        price:'1200'
+        price: '1200',
+        image: 'https://res.cloudinary.com/digem679d/image/upload/v1668459527/ddgb2knhcsqfd06z9amt.jpg'
     },
     {
         id: '4',
         name: 'Mpishi',
         Size: '6',
-        price:'1200'
+        price: '1200',
+        image: 'https://res.cloudinary.com/digem679d/image/upload/v1668459526/qu9b3ab1ne3mxhj5firm.jpg'
     },
     {
         id: '5',
         name: 'Ambazo',
         Size: '6',
-        price:'1200'
+        price: '1200',
+        image: 'https://res.cloudinary.com/digem679d/image/upload/v1668459526/jbv2hocydtzgwf3wozaa.jpg'
     },
     {
         id: '6',
         name: 'Pika',
         Size: '6',
-        price:'1200'
+        price: '1200',
+        image: 'https://res.cloudinary.com/digem679d/image/upload/v1668459526/jbv2hocydtzgwf3wozaa.jpg'
     }
 ]
 
