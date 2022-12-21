3df52ad9b77729013efad40e070b2868e4561f73
jacob-d242 <jacobmoracha@gmail.com>
2022-12-21 17:19:04 +0300

    updating the shop part

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,10 +1,17 @@
 import { HStack } from 'native-base';
 import React from 'react';
+import  Icon  from 'react-native-vector-icons/FontAwesome';
 
-export default function Rating() {
+export default function Rating({value}) {
+    const size = 30
+
     return (
         <HStack space={0.4} mt={2} alignItems='center'>
-
+            <Icon
+                size={size}
+                name={value >= 1 ? 'star' : value >= 0.5 ? 'star-haif-o' : 'start-haif-o'}
+                color='#FFD700'
+            />
         </HStack>
 
     );
