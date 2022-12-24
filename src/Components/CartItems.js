74a2981818c7fe8df9a3805b39768685689922e8
jacob-d242 <jacobmoracha@gmail.com>
2022-12-24 12:10:03 +0300

    creating cart itemsa component

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -1,10 +1,60 @@
-import { Box, Text } from 'native-base'
+import { Box, HStack, Pressable, Text } from 'native-base'
 import React from 'react'
+import { SwipeListView } from 'react-native-swipe-list-view'
+import gasData from '../data/ProductData'
+import { COLORS } from '../Constants/Index'
+
+const Swiper = () => {
+  <SwipeListView
+    rightOpenValue={-50}
+   // previewRowKey={0}
+    previewOpenValue={-40}
+    previewOpenDelay={3000}
+    data={gasData.slice(0, 2)}
+    renderItem={}
+    renderHiddenItem={}
+    showsVerticalScrollIndicator={false}
+  />
+};
+
+const renderIterms = (data) => {
+  <Pressable>
+    <Box ml={6} mb={3}>
+
+      <HStack
+        alignItems='center'
+        bg={COLORS.backgrounds}
+        shadow={1}
+        rounded={10}
+        overflow='hidden'
+      >
+        <Text>fghjkl</Text>
+      </HStack>
+    </Box>
+  </Pressable>
+}
+const renderHiddenIterms = () => {
+  <Pressable>
+    <Box ml={6} mb={3}>
+
+      <HStack
+        alignItems='center'
+        bg={COLORS.backgrounds}
+        shadow={1}
+        rounded={10}
+        overflow='hidden'
+      >
+        <Text>fghjkl</Text>
+      </HStack>
+    </Box>
+  </Pressable>
+}
 
 export default function CartItems() {
+  
   return (
-      <Box>
-          <Text>Cart products</Text>
+    <Box mr={3}>
+          <Swiper/>
    </Box>
   )
 }
