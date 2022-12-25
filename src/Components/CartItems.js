d19a5883bbe0930848ce563ca14a11ff2a5a8618
jacob-d242 <jacobmoracha@gmail.com>
2022-12-25 22:44:14 +0300

    creating csrt component

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -1,23 +1,24 @@
-import { Box, HStack, Pressable, Text } from 'native-base'
+import { Box, Center, HStack, Image, Pressable, Text, VStack } from 'native-base'
 import React from 'react'
 import { SwipeListView } from 'react-native-swipe-list-view'
 import gasData from '../data/ProductData'
 import { COLORS } from '../Constants/Index'
+import Icon  from 'react-native-vector-icons/FontAwesome'
 
-const Swiper = () => {
+const Swiper = () => (
   <SwipeListView
     rightOpenValue={-50}
-   // previewRowKey={0}
+    previewRowKey='0'
     previewOpenValue={-40}
     previewOpenDelay={3000}
-    data={gasData.slice(0, 2)}
-    renderItem={}
-    renderHiddenItem={}
+    data={gasData.slice(0, 3)}
+    renderItem={renderItems}
+    renderHiddenItem={hiddenItem}
     showsVerticalScrollIndicator={false}
   />
-};
+)
 
-const renderIterms = (data) => {
+const renderItems = (gasData) => (
   <Pressable>
     <Box ml={6} mb={3}>
 
@@ -28,33 +29,43 @@ const renderIterms = (data) => {
         rounded={10}
         overflow='hidden'
       >
-        <Text>fghjkl</Text>
+        <Center w='25%' bg={COLORS.main }>
+          <Image
+            source={{ uri: gasData.item.image }}
+            alt={gasData.item.name}
+            w='full'
+            resizeMode='contain'
+          />
+        </Center>
+        <VStack w='60%' px={2} space={3}>
+          <Text isTruncated bold color={COLORS.main} fontSize={ 18}>{gasData.item.name }</Text>
+          <Text  bold color={COLORS.main} fontSize={18}>KSHS{gasData.item.price }</Text>
+        </VStack>
       </HStack>
     </Box>
   </Pressable>
-}
-const renderHiddenIterms = () => {
-  <Pressable>
-    <Box ml={6} mb={3}>
+)
 
-      <HStack
-        alignItems='center'
-        bg={COLORS.backgrounds}
-        shadow={1}
-        rounded={10}
-        overflow='hidden'
-      >
-        <Text>fghjkl</Text>
-      </HStack>
-    </Box>
+const hiddenItem = () => (
+  <Pressable
+    w={50}
+    roundedTopRight={10}
+    roundedBottomRight={10}
+    h='88%'
+    ml='auto'
+    justifyContent='center'
+    bg='red'>
+    <Center alignItems='center' space={2} >
+      <Icon name='trash' size={28} color={COLORS.main } />
+    </Center>
   </Pressable>
-}
+)
 
 export default function CartItems() {
-  
+
   return (
-    <Box mr={3}>
-          <Swiper/>
-   </Box>
+    <Box mr={6} >
+      <Swiper />
+    </Box>
   )
 }
