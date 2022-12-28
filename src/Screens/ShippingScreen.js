b390a8d05a3a602180be18a8a6989f0fc0fea778
jacob-d242 <jacobmoracha@gmail.com>
2022-12-28 10:12:21 +0300

    Payment Screen Complete

--- src/Screens/ShippingScreen.js
+++ src/Screens/ShippingScreen.js
@@ -1,15 +1,69 @@
-import { Box, Center, Text } from 'native-base'
+import { Box, Center, FormControl, Input, Text, VStack } from 'native-base'
 import React from 'react'
 import { COLORS } from '../Constants/Index'
+import MinBtn from '../Components/MinBtn'
+
+const ShippingInputs = [
+    {
+        label: 'COUNTY',
+        type:'text'
+    },
+    {
+        label: 'STREET',
+        type:'text'
+    },
+    {
+        label: 'HOUSE NO',
+        type:'text'
+    },
+    {
+        label: 'PHONE NUMBER',
+        type:'text'
+    }
+]
+
 export default function ShippingScreen() {
     return (
-        <Box flex={1} safeAreaTop bg={COLORS.primary}>
+        <Box flex={1} py={5} safeAreaTop bg={COLORS.primary}>
             <Center pb={15}>
 
                 <Text color={COLORS.white} fontSize={22} bold>Shipping</Text>
             </Center>
-            <Box h='full' bg={COLORS.white}>
+            
+            <Box h='full'px={5} bg={COLORS.white}>
+                <VStack space={6} mt={5}>
+                {
+                ShippingInputs.map((i,index) => (
+                    <FormControl
+                        key={index}
+                    >
+                        <FormControl.Label
+                            _text={{
+                                fontSize: '14px',
+                                fontWeight:'bold'
+                            }}
+                        >
+                            {i.label}
+                        </FormControl.Label>
+                        <Input
+                            borderWidth={0.3}
+                            borderColor={COLORS.main}
+                            py={4}
+                            bg={COLORS.backgrounds}
+                            type={i.type}
+                            _focus={{
+                               // bg:COLORS.lightPrimary
+                                borderWidth:1
+                            }}
+                        />
+                    </FormControl>
+                ))
+            }
 
+                    <MinBtn mt={5}  bg={COLORS.primary} color={COLORS.white} >
+                        CONTINUE
+                    </MinBtn> 
+                </VStack>
             </Box>
         </Box>
     )
